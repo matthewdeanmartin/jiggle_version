89d99efb2025f2a91d83b4a45508f60771e7b757
Matthew Martin <matthewdeanmartin@gmail.com>
2018-07-11 17:26:07 -0400

    version change noise

--- jiggle_version/__init__.py
+++ jiggle_version/__init__.py
@@ -2,4 +2,4 @@
 """
 Opinionated version incrementing
 """
-__version__ = "1.0.7"
+__version__ = "1.0.9"
--- sample_lib/__init__.py
+++ sample_lib/__init__.py
@@ -1,4 +1,4 @@
 """
 Sample library for tests to target.
 """
-__version__ = "1.0.28"
+__version__ = "1.0.30"
