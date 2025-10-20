b84cb4fc0a8beb8b3fdbd015e1998219b9433147
Matthew Martin <matthewdeanmartin@gmail.com>
2025-10-20 18:38:47 -0400

    2.1.0: missing packaging library

--- jiggle_version/__about__.py
+++ jiggle_version/__about__.py
@@ -8,12 +8,14 @@ __all__ = [
     "__keywords__",
     "__license__",
     "__requires_python__",
+    "__status__",
 ]
 
 __title__ = "jiggle_version"
-__version__ = "2.0.1"
+__version__ = "2.1.0"
 __description__ = "Increment version number found in source code without regex"
 __readme__ = "README.md"
 __keywords__ = ["version", "version-numbers"]
 __license__ = "MIT"
 __requires_python__ = ">=3.8"
+__status__ = "3 - Alpha"
